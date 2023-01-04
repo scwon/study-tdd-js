3af35a44b24a01ffc6546da0c463c1a311f09830
scwon <a49297466@gmail.com>
2023-01-04 23:22:24 +0900

    새로운 테스트 케이스 추가

--- step1_multi_currency/Dollar.ts
+++ step1_multi_currency/Dollar.ts
@@ -4,7 +4,7 @@ class Dollar {
     this.amount = amount;
   }
   times = (multiplier: number) => {
-    this.amount *= multiplier;
+    return new Dollar(this.amount * multiplier);
   };
 }
 
--- step1_multi_currency/index.test.ts
+++ step1_multi_currency/index.test.ts
@@ -1,7 +1,18 @@
 import Dollar from "./Dollar";
 
-test("달러 곱셈", () => {
-  const five = new Dollar(5);
-  five.times(2);
-  expect(five.amount).toEqual(10);
+describe("화폐 계산", () => {
+  test("달러 곱셈", () => {
+    const five = new Dollar(5);
+    const product = five.times(2);
+    expect(product.amount).toEqual(10);
+  });
+
+  test("달러 곱셈 여러번", () => {
+    const five = new Dollar(5);
+    let product = five.times(2);
+    expect(product.amount).toEqual(10);
+
+    product = five.times(3);
+    expect(product.amount).toEqual(15);
+  });
 });
