8cd27429f113f8a08757565c1c5e94ca21f13fb9
scwon <a49297466@gmail.com>
2023-01-05 22:27:56 +0900

    삼각측량을 통한 작성

--- step1_multi_currency/Dollar.ts
+++ step1_multi_currency/Dollar.ts
@@ -3,8 +3,13 @@ class Dollar {
   constructor(amount: number) {
     this.amount = amount;
   }
+
   times = (multiplier: number) => {
     return new Dollar(this.amount * multiplier);
+  };
+
+  equals = (dollar: Dollar) => {
+    return this.amount === dollar.amount;
   };
 }
 
--- step1_multi_currency/index.test.ts
+++ step1_multi_currency/index.test.ts
@@ -10,9 +10,14 @@ describe("화폐 계산", () => {
   test("달러 곱셈 여러번", () => {
     const five = new Dollar(5);
     let product = five.times(2);
-    expect(product.amount).toEqual(10);
+    expect(product.amount).toBe(10);
 
     product = five.times(3);
-    expect(product.amount).toEqual(15);
+    expect(product.amount).toBe(15);
+  });
+
+  test("같은지 확인", () => {
+    expect(new Dollar(5).equals(new Dollar(5))).toBeTruthy();
+    expect(new Dollar(5).equals(new Dollar(6))).toBeFalsy();
   });
 });
