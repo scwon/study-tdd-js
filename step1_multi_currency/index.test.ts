52a92ef5b209c5f8ed8cf8918067032f96708ed5
scwon <a49297466@gmail.com>
2023-01-05 23:03:46 +0900

    value 패턴 느낌나는 테스트로 개선

--- step1_multi_currency/index.test.ts
+++ step1_multi_currency/index.test.ts
@@ -3,17 +3,13 @@ import Dollar from "./Dollar";
 describe("화폐 계산", () => {
   test("달러 곱셈", () => {
     const five = new Dollar(5);
-    const product = five.times(2);
-    expect(product.equals(new Dollar(10))).toBeTruthy();
+    expect(five.times(2).equals(new Dollar(10))).toBeTruthy();
   });
 
   test("달러 곱셈 여러번", () => {
     const five = new Dollar(5);
-    let product = five.times(2);
-    expect(product.equals(new Dollar(10))).toBeTruthy();
-
-    product = five.times(3);
-    expect(product.equals(new Dollar(15))).toBeTruthy();
+    expect(five.times(2).equals(new Dollar(10))).toBeTruthy();
+    expect(five.times(3).equals(new Dollar(15))).toBeTruthy();
   });
 
   test("같은지 확인", () => {
